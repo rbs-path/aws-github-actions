1385338f14d9e9f1338b24c2d96be54bcb1cca3e
Zsolt Fejer <60187881+ZsoltPath@users.noreply.github.com>
2021-10-05 14:49:03 +0100

    Update packages/cloudfront_invalidate/index.ts
    Co-authored-by: Vaughan Reid <vaunus@gmail.com>

--- packages/cloudfront_invalidate/index.ts
+++ packages/cloudfront_invalidate/index.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core';
 import * as CloudFront from 'aws-sdk/clients/cloudfront';
-import * as AWS from 'aws-sdk';
 
 const run = async (): Promise<void> => {
   try {
